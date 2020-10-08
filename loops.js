8a9f16816d123050dc79d33abb4779b4260443e6
Zachary Perkins <ezmac028@gmail.com>
2020-10-08 02:22:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,3 +15,12 @@ function whileLoop(n) {
   }
   return 'done'
 }
+
+function doWhileLoop(num) {
+  num = num + 1;
+  return num
+}
+
+do {
+  console.log("I run once regardless.")
+} while (doWhileLoop() < 10)
