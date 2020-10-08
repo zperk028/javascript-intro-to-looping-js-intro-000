6b433fa04ce5b11d2b015bab66823ad2e38678ef
Zachary Perkins <ezmac028@gmail.com>
2020-10-08 00:57:11 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,8 @@
-let array = 0
-
 function forLoop(array) {
   for (let i = 0; i < 26; i++) {
+    if (i === 1) {
+      array.push(`I am 1 strange loop`)
+    } else {
     array.push(`I am ${i} strange loops.`)
   }
 }
